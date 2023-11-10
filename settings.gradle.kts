db2494c4088f513e6c46399fcc315b6fdaf934e3
Mario Ruiz Castillo <91901698+MarioRC312@users.noreply.github.com>
2023-11-10 12:11:36 +0100

    primera subida

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "projecte_JanF_MarioR"
+include(":app")
