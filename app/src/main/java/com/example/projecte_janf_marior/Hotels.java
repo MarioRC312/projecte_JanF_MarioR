c16ee2264b92844ca55d955bb5c133f7a17f5624
jan ferrer <zenter-puz@gmail.com>
2023-11-13 09:59:12 +0100

    cambios en hotel.xml

---
+++ app/src/main/java/com/example/projecte_janf_marior/Hotels.java
@@ -0,0 +1,14 @@
+package com.example.projecte_janf_marior;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Hotels extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurants);
+    }
+}
