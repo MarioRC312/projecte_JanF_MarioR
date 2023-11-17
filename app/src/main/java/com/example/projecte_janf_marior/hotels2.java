0f145017a4171842ac4f1dc9aab72650baa60360
jan ferrer <zenter-puz@gmail.com>
2023-11-17 12:25:54 +0100

    cambios en hotel.xml

--- app/src/main/java/com/example/projecte_janf_marior/hotels2.java
+++ app/src/main/java/com/example/projecte_janf_marior/hotels2.java
@@ -1,14 +1,14 @@
 package com.example.projecte_janf_marior;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Hotels extends AppCompatActivity {
+import android.os.Bundle;
+
+public class hotels2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_restaurants);
+        setContentView(R.layout.activity_hotels2);
     }
 }
