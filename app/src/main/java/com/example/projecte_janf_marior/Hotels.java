64cb8a3c01efada391337bb05258c9d2864a525c
jan ferrer <zenter-puz@gmail.com>
2023-11-29 20:08:37 +0100

    modificacions

---
+++ app/src/main/java/com/example/projecte_janf_marior/Hotels.java
@@ -0,0 +1,23 @@
+package com.example.projecte_janf_marior;
+
+public class Hotels {
+
+
+    private String valoracio;
+
+    public Hotels(String valoracio) {
+
+
+        this.valoracio=valoracio;
+    }
+
+    public String getValoracio() {
+        return valoracio;
+    }
+
+    public void setValoracio(String valoracio) {
+        this.valoracio = valoracio;
+    }
+}
+
+
